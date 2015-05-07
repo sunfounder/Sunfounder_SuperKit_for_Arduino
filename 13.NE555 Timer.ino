69e426301f35a47eb9886b45b866b2d380b9f466
SunFounder <support@sunfounder.com>
2015-05-07 15:11:27 +0800

    Create 13.NE555 Timer.ino

---
+++ 13.NE555 Timer.ino
@@ -0,0 +1,21 @@
+//NE555 Timer
+//After burning the program, open the serial monitor,you can see that if you rotate the potentiometer, the length of the pulse (in microsecond) displayed will change accordingly.
+//Email:support@sunfounder.com
+//Website:www.sunfounder.com
+//2015.5.7
+int pin = 7;  //attach to the third pin of NE555
+unsigned long duration;  //the variable to store the length of the pulse 
+ 
+void setup()
+{
+  pinMode(pin, INPUT);  //set the pin as an input
+  Serial.begin(9600);  // start serial port at 9600 bps:
+}
+ 
+void loop()
+{
+  duration = pulseIn(pin, HIGH);  //Reads a pulse on pin
+  Serial.print(duration);  //print the length of the pulse on the serial monitor
+  Serial.println();  //print an blank on serial monitor
+  delay(500);  //wait for 500 microseconds
+}
